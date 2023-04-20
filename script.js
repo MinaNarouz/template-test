c93aa666463735c4d477c4c869a32140721110ea
Mina Nady <minanarouz666@gmail.com>
2023-04-21 01:04:26 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+// In JS you can use WINDOW which is very useful in interacting with browser interactions with user.
+// Also you can use window.console.log to display things in browser which is useful for debugging and seeing what an API data is.
+
+console.log("testing");
